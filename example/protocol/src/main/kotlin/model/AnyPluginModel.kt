7d07be2c72df5866a88e7d604c8438cee89b2829
Rustam Sayfutdinov <rstm.sf@gmail.com>
2020-11-25 00:00:15 +0300

    Add text property to rd model

--- example/protocol/src/main/kotlin/model/AnyPluginModel.kt
+++ example/protocol/src/main/kotlin/model/AnyPluginModel.kt
@@ -1,11 +1,14 @@
 package model
 
 import com.jetbrains.rd.generator.nova.Ext
+import com.jetbrains.rd.generator.nova.PredefinedType
+import com.jetbrains.rd.generator.nova.property
 import com.jetbrains.rider.model.nova.ide.SolutionModel
 
 @Suppress("unused")
 object AnyPluginModel : Ext(SolutionModel.Solution) {
 
     init {
+        property("text", PredefinedType.string)
     }
 }
