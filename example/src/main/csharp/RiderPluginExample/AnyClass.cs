6fd4cd17ff3dcfab6b1f4e8ec5675232bb570c52
Rustam Sayfutdinov <rstm.sf@gmail.com>
2020-11-25 00:16:40 +0300

    Init rd-model text property in backend

---
+++ example/src/main/csharp/RiderPluginExample/AnyClass.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.ProjectModel;
+using JetBrains.Rd.Base;
+using JetBrains.ReSharper.Host.Features;
+using JetBrains.Rider.Model;
+using JetBrains.Util;
+
+namespace RiderPluginExample
+{
+    [SolutionComponent]
+    public class AnyClass
+    {
+        public AnyClass(ISolution solution, ILogger logger)
+        {
+            var rdModel = solution.GetProtocolSolution().GetAnyPluginModel();
+
+            logger.Info(solution.Name + ": " + rdModel.Text.Maybe.ValueOrDefault);
+
+            rdModel.Text.Set(" ☜(ﾟヮﾟ☜)");
+        }
+    }
+}
